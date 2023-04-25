211a25daabcc285fc0f9a753c6b28a22eaac9ccd
Popov Nicolai <110418111+psyrut1983@users.noreply.github.com>
2023-04-25 16:32:32 +0300

    доработал экран авторизации и регистрации

--- app/src/main/java/com/example/cinema/MainActivity.kt
+++ app/src/main/java/com/example/cinema/MainActivity.kt
@@ -5,35 +5,64 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private val signInLauncher = registerForActivityResult( //создали объект авторизации
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)  //запуск экрана
+    }
+    private lateinit var database: DatabaseReference
+
+    val providers = arrayListOf(
+        AuthUI.IdpConfig.EmailBuilder().build(),
+    )
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
-
-
-
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-val registerButton = findViewById<Button>(R.id.main_activity_registration_button)
-        val loginText = findViewById<TextView>(R.id.main_activity_login_text)
+        database = Firebase.database.reference   //инциализация базы данных
 
-        registerButton.setOnClickListener {
-            val intentToRegistrationActivity = Intent(this, RegistrationActivity::class.java)
-            startActivity(intentToRegistrationActivity)
-
-        }
-
-     loginText.setOnClickListener {
-         val intentToLoginActivity = Intent(this, LoginActivity::class.java)
-         startActivity(intentToLoginActivity)
-
-
-     }
-
-
-
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
     }
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            val authuser = FirebaseAuth.getInstance().currentUser
 
+            authuser?.let {
+                val email = it.email.toString()
+                val uid = it.uid
+                val firebasUser = User(email, uid)
+                database.child("users").child(uid).setValue(firebasUser)
+                val intentToMoviesActivity = Intent(this, MoviesActivity::class.java)
+                startActivity(intentToMoviesActivity)
+
+            }
+
+
+
+            // ...
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+        }
+    }
 
 
 
---
+++ app/src/main/java/com/example/cinema/MoviesActivity.kt
@@ -0,0 +1,18 @@
+package com.example.cinema
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoviesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+
+
+
+
+    }
+
+
+}
